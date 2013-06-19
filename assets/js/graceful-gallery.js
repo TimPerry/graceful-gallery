7745dc7e71ab577cb73d90932beaed53936dbfab
Tim Perry <tim.r.perry@gmail.com>
2013-06-19 23:40:39 +0100

    Removed use of globals, moved to  object for storage.

--- assets/js/graceful-gallery.js
+++ assets/js/graceful-gallery.js
@@ -1,163 +1,159 @@
 /*
-*
-* Graceful gallery
-* by Tim Perry (www.applicious.co)
-* v1.0 - April 2013
-* 
-*/
+ *
+ * Graceful gallery
+ * by Tim Perry (www.applicious.co)
+ * v1.0 - April 2013
+ *
+ */
 (function($) {
 
-	$.fn.GracefulGallery = function( args ) {
+	$.fn.GracefulGallery = function(args) {
 
 		var opts = $.extend({
 
 			"current_class": "is-current",
-			"pagination_classes" : "",
-			"slider_speed" : 1000,
-			"slider_timeout" : 2000,
-			"disable_css_animations" : false
+			"pagination_classes": "",
+			"slider_speed": 1000,
+			"slider_timeout": 5000,
+			"disable_css_animations": false
 
-		}, args);
+		}, args );
+		
+		var methods = {
+			
+			slide : function( $slider, should_increment ) {
+					
+				clearTimeout( $slider.timer );
+				
+				// check if we needs to increment
+				if( should_increment && ( ++$slider.page_num >= $slider.pages_count ) ) {
+					$slider.page_num = 0;
+				}
+				
+	            // force the class
+	            $( $slider.pagination ).siblings( ).removeClass( opts.current_class ).eq( $slider.page_num ).addClass( opts.current_class );
+				
+				// calc the new left value
+				var left = $slider.page_width * $slider.page_num;
+				
+				// perform the slide
+				if ( ! opts.disable_css_animations && typeof window.Modernizr !== 'undefined' && Modernizr.csstransitions ) {
 
+					$slider.pages_holder.css( "transform", [ "translate(-", left, "px)" ].join( "" ) );
+					$slider.pages_holder.css( "transition", [ opts.slider_speed + "ms cubic-bezier( 0.25, 0.1, 0.25, 1)" ].join( "" ) );
+
+				} else {
+
+					$slider.pages_holder.animate({
+						'left': [ "-", left, "px" ].join( "" )
+					}, opts.slider_speed);
+
+				}
+				
+				// slide again in x seconds
+				$slider.timer = setTimeout( function() {
+					methods.slide( $slider, true );					
+				}, opts.slider_timeout );
+				
+			},
+			update_widths : function( $slider ) {
+				
+				$slider.page_width = $slider.outerWidth();
+				$slider.pages.width( $slider.page_width );
+				$slider.pages_holder.width( $slider.page_width * $slider.pages_count );
+				
+			},
+			setup_attrs : function( $slider ) {
+			
+				// setup slider attrs
+				$slider.pages_holder = $slider.find( ".slider-pages" );
+				$slider.pages = $slider.pages_holder.children( );
+				$slider.pagination = false; // setup in setup_pagination
+				
+				$slider.page_width = false // setup laster in update widths
+				$slider.pages_count = $slider.pages.length;
+				$slider.timer = 0;
+				$slider.page_num = 0;
+				
+				// setup the width
+				methods.update_widths( $slider );
+				
+			},
+			setup_pagination : function( $slider ) {
+								
+				var i = 1,
+		        	html = [
+
+			        '<div class="slider-pagination"><a class="',
+			        opts.pagination_classes,
+			        ' ',
+			        opts.current_class, '" href="#">1</a>'
+
+		        ];
+
+				// generate the pagination markup
+				while ( i++ < $slider.pages_count ) {
+
+					html.push( '<a class="' );
+					html.push( opts.pagination_classes );
+					html.push( '" href="#">' );
+					html.push( i );
+					html.push( '</a>' );
+
+				}
+
+				html.push( '</div>' );
+				$( $slider ).append( html.join( "" ) );
+				
+				// we can now setup the pagination
+				$slider.pagination = $slider.find( '.slider-pagination' ).find( 'a' );	
+				
+			},
+			setup_listeners : function( $slider ) {
+				
+				// setup click listener
+				$slider.pagination.click( function( e ) {
+										
+					$slider.page_num = $( this ).index( );
+					methods.slide( $slider, false );
+					
+					e.preventDefault( );
+
+				});
+			
+				// update widths on resize
+				$( window ).resize( function( ) {
+					
+					// update widths
+					methods.update_widths( $slider );
+					$slider.pages_holder.css( 'left', [ "-", ( $slider.page_width * $slider.page_num ), "px" ].join( "" ) );
+					
+				});
+				
+		        // pause on hover
+		        $( $slider ).hover( function( ) {
+		        	clearTimeout( $slider.timer );
+		        }, function( ) {
+		        	methods.slide( $slider, false );
+		        });
+
+			}
+
+		};
+
+		// setup each slider
 		return this.each( function( ) {
 
-			var globals = {
+			// cache the slider
+			var $slider = $( this );
+
+			// set everything up
+			methods.setup_attrs( $slider );
+			methods.setup_pagination( $slider );
+			methods.setup_listeners( $slider );
 			
-				self : $( this ),
-				width : $( this ).outerWidth( ),
-				pagination : false,
-				pages_holder : $( this ).find( ".slider-pages" ),
-				pages : null,
-				total_pages : null,
-				slider_timer : 0,
-				index : -1,
-				i : 1,
-				html : [
-
-					'<div class="slider-pagination"><a class="',
-					opts.pagination_classes,
-					' ',
-					opts.current_class,
-					'" href="#">1</a>'
-
-				]				
-			};
-            
-            // setup the extra variables now we have all the data
-            globals.pages = $( globals.pages_holder ).children();
-            globals.total_pages = globals.pages.length;
-
-			var methods = {
-				
-				slide : function( increment ) {
-					 
-                    increment = increment || true;
-                     
-                    // first things first, clear the timeout     
-                    clearTimeout( globals.slider_timer );
-                    
-                    // force the class
-					$( globals.pagination ).siblings( ).removeClass( opts.current_class ).eq( globals.index ).addClass( opts.current_class );
-                    
-                    // now increment our index
-					if ( increment && ( ++globals.index >= globals.total_pages ) ) {
-						globals.index = 0;
-					}
-                    
-                    // caclulate where we should slide to
-                    var left = globals.index * globals.width;
-                                    		
-                    // perform the slide
-					if ( ! opts.disable_css_animations && typeof window.Modernizr !== 'undefined' && Modernizr.csstransitions ) {
-
-						globals.pages_holder.css( 'transform', [ "translate(-", left, "px)" ].join("") );
-						globals.pages_holder.css( 'transition', [ opts.slider_speed + 'ms cubic-bezier( 0.25, 0.1, 0.25, 1)' ].join("") );
-
-					} else {
-
-						globals.pages_holder.animate({ 'left': [ "-", left, "px" ].join("") }, opts.slider_speed );
-
-					}
-
-					// restart timer
-					globals.slider_timer = setTimeout( function() {
-
-						methods.slide( false );
-
-					}, opts.slider_timeout );
-
-				},
-				update_widths : function() {
-
-                    // update the width used for each page
-					globals.width = globals.self.outerWidth();
-                    
-                    // force the width
-					globals.pages.width( globals.width );
-                    
-                    // make the pages holder wide enough for all the pages
-					globals.pages_holder.width( globals.width * globals.total_pages );
-
-				},
-				setup_pagination : function( ) {
-				
-					// generate the pagination markup
-					while( globals.i < globals.total_pages ) {
-					        
-                            globals.i++;
-
-                            globals.html.push( '<a class="' );
-							globals.html.push( opts.pagination_classes );
-							globals.html.push( '" href="#">' );
-							globals.html.push( globals.i );
-							globals.html.push( '</a>' );
-
-					}
-					
-					globals.html.push( '</div>' );
-					$( globals.self ).append( globals.html.join("") );
-					globals.pagination = $( globals.self ).find( '.slider-pagination a' );
-
-					var click_function = function( ) {
-
-						globals.index = $( this ).index( );
-						methods.slide( );
-
-						return false;
-
-					};
-			
-					// setup pagination
-					if ( $.isFunction( $.fn.on ) ) {
-						$( globals.pagination ).on( 'click', click_function );	
-					} else {
-						$( globals.pagination ).live( 'click', click_function );
-					}
-					
-				}
-			
-			};
-
-			// update width on resize
-			$( window ).resize( function( ) {
-				methods.update_widths();
-			});
-
-			// pause on hover
-			//$( globals.self ).hover( function( ) {
-			//	clearTimeout( globals.slider_timer );
-			//}, function( ) {
-			//	methods.slide( );
-			//});
-			
-			// setup pagination
-			methods.setup_pagination();
-			
-			// setup the widths
-			methods.update_widths();
-			
-			// start up
-			methods.slide( );
+			// start animating
+			methods.slide( $slider, false );
 
 		});
 
